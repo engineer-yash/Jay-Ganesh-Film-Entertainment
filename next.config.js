6d6647df8cc7d9b948142d94f64d44e1a0f3ae1a
Yash Gohel <gohelyash94@gmail.com>
2026-07-16 14:35:38 +0530

    fixed changes

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "cdninstagram.com" },
+      { protocol: "https", hostname: "scontent.cdninstagram.com" },
+    ],
+  },
+};
+
+module.exports = nextConfig;
